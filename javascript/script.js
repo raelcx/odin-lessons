46aa26d2823e4c8aa72a5fe15bc18f3aaffa2fd7
Israel Carvalho <77789295+raelcx@users.noreply.github.com>
2024-12-05 19:31:19 -0300

    Solve some more exercises

--- javascript/script.js
+++ javascript/script.js
@@ -1,17 +1,9 @@
-function isOdd(num) {
-    return num % 2 !== 0;
-  }
-  const arr = [1, 2, 3, 4, 5];
-  const oddNums = arr.filter(isOdd);
-  console.log(oddNums); // Outputs [1, 3, 5];
-  console.log(arr); // Outputs [1, 2, 3, 4, 5], original array is not affected
+let john = { name: "John", age: 25 };
+let pete = { name: "Pete", age: 30 };
+let mary = { name: "Mary", age: 28 };
 
-  
+let users = [ john, pete, mary ];
 
-  const arr = [1, 2, 3, 4, 5];
-  const productOfAllNums = arr.reduce((total, currentItem) => {
-    return total * currentItem;
-  }, 1);
-  console.log(productOfAllNums); // Outputs 120;
-  console.log(arr); // Outputs [1, 2, 3, 4, 5]
-  
+let names = users.map(item => item.name);
+
+alert( names ); // John, Pete, Mary
